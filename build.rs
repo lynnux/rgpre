2a4e9564cd815af97b1a04d59b975412f5e7014e
lynnux <lynnux@qq.com>
2022-03-12 21:51:02 +0800

    try fix build

--- build.rs
+++ build.rs
@@ -6,7 +6,10 @@ fn main() {
         .file("src/main.cpp")
         .compile("foo");
 
-    println!("cargo:rustc-link-arg=foo.lib"); // vs2013可能需要这个，试了很多办法都失败
+    #[cfg(windows)]
+    {
+        println!("cargo:rustc-link-arg=foo.lib"); // vs2013可能需要这个，试了很多办法都失败
+    }
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/main");
 }
